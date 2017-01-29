aac2a845b04e85ab6778902c087aa96ca8515ef5
PANOPTES\mbosc <bosko.mo@gmail.com>
2017-01-29 02:50:37 +0100

    no message

--- BlocchiRidimensionabili/Assets/Scripts/Model/MouthBlock.cs
+++ BlocchiRidimensionabili/Assets/Scripts/Model/MouthBlock.cs
@@ -5,6 +5,8 @@ namespace model
     public abstract class MouthBlock : Block
     {
         protected Block innerNext;
+        private string valString;
+
 
         public void SetInnerNext(Block block)
         {
---
+++ BlocchiRidimensionabili/Assets/Scripts/editorfr.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono;
+using System;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Net;
+
+using System.CodeDom.Compiler;
+
+using UnityEditor;
+
+[CustomEditor(typeof(welo))]
+[CanEditMultipleObjects]
+public class editorfr : Editor
+{
+    SerializedProperty lookAtPoint;
+
+    void OnEnable()
+    {
+       
+    }
+
+    private bool vis = false;
+    private string code1 = @"
+namespace model {
+    public class ";
+    private string code2 = @" {
+public string hey(){";
+    private string code3 = "} } }";
+    private string nombre, code;
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUILayout.LabelField("Class Name");
+        nombre = EditorGUILayout.TextArea(nombre);
+        EditorGUILayout.LabelField("Execute()");
+        code = EditorGUILayout.TextArea(code);
+        if (GUILayout.Button("Compute"))
+            vis = !vis;
+        if (vis)
+            EditorGUILayout.TextArea(System.Environment.Version.ToString());
+        serializedObject.ApplyModifiedProperties();
+        
+
+    }
+}
+
